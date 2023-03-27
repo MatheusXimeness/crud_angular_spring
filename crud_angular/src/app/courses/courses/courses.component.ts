3a52977e378cbe79788c4544c8a6aa1fc4535026
Matheus Ferreira <matheus.ferreira@bitka.com.br>
2023-03-27 13:41:50 -0300

    instanciando serviço

--- crud_angular/src/app/courses/courses/courses.component.ts
+++ crud_angular/src/app/courses/courses/courses.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Course } from '../model/course';
+import { CoursesService } from '../services/courses.service';
 
 @Component({
   selector: 'app-courses',
@@ -8,13 +10,14 @@ import { Course } from '../model/course';
 })
 export class CoursesComponent implements OnInit {
 
-  courses: Course[] = [ // variavel ganhou o tipo do model que foi criado
-    { _id: '1', name: 'Angular', category: 'front-end' }
-  ];
+  courses: Course[] = []; // variavel ganhou o tipo do model que foi criado
+
 
   displayedColumns = ['name', 'category'];
 
-  constructor() { }
+  constructor(private coursesService: CoursesService) {
+    this.courses = this.coursesService.list();
+  }
 
   ngOnInit(): void {
 
