139ce268ce2be535b19e84d037b05e0afdcbc704
Matheus Ferreira <matheus.ferreira@bitka.com.br>
2023-03-27 14:19:06 -0300

    fazendo requisição GET http para a API que foi criada (assets)

--- crud_angular/src/app/courses/services/courses.service.ts
+++ crud_angular/src/app/courses/services/courses.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { first, tap } from 'rxjs/operators';
 
 import { Course } from '../model/course';
 
@@ -8,11 +9,15 @@ import { Course } from '../model/course';
 })
 export class CoursesService {
 
+  private readonly API = '/assets/courses.json';
+
   constructor(private httpClient: HttpClient) { }
 
-  list(): Course[] {
-    return [
-      { _id: '1', name: 'Angular', category: 'front-end' }
-    ];
+  list() {
+    return this.httpClient.get<Course[]>(this.API) // esse é um observable que retorna um array de Courses
+    .pipe(
+      first(), // pega o primeiro dado que o endpoint retornar e encerra a conexão
+      tap(courses => console.log(courses))
+    );
   }
 }
