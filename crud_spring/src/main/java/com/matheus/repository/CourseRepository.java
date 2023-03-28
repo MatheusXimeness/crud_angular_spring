10f86dede8256645a6b72436a72c3fc4ac4821f8
Matheus Ferreira <matheus.ferreira@bitka.com.br>
2023-03-28 11:16:42 -0300

    criando interface de acesso a base de dados

--- crud_spring/src/main/java/com/matheus/repository/CourseRepository.java
+++ crud_spring/src/main/java/com/matheus/repository/CourseRepository.java
@@ -1,2 +1,10 @@
-package com.matheus.repository;public class CourseRepository {
+package com.matheus.repository;
+
+import com.matheus.model.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CourseRepository extends JpaRepository<Course, Long> {
+
 }
