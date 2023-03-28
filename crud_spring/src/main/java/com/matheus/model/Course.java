37a086d233df2309a82aca86c980c5b374fe6feb
Matheus Ferreira <matheus.ferreira@bitka.com.br>
2023-03-28 11:16:24 -0300

    criando acesso a base de dados

--- crud_spring/src/main/java/com/matheus/model/Course.java
+++ crud_spring/src/main/java/com/matheus/model/Course.java
@@ -1,2 +1,21 @@
-package com.matheus.model;public class Course {
+package com.matheus.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity // nesse caso utilizando entity é criada uma tabela com o nome dessa classe no BD
+//@Table(name = "cursos") - isso é p/ caso ja tenha um bd legado e vc deseja ref sua table
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(length = 200, nullable = false)
+    private String name;
+
+    @Column(length = 10, nullable = false)
+    private String category;
 }
