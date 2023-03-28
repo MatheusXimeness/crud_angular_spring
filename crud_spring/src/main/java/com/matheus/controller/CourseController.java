952b0c81003345873bac8b7941d5dd2c7026b3c3
Matheus Ferreira <matheus.ferreira@bitka.com.br>
2023-03-28 11:16:06 -0300

    criando controlador que vai receber as requisições HTTP

--- crud_spring/src/main/java/com/matheus/controller/CourseController.java
+++ crud_spring/src/main/java/com/matheus/controller/CourseController.java
@@ -1,2 +1,24 @@
-package com.matheus.controller;public class CourseController {
+package com.matheus.controller;
+
+import com.matheus.model.Course;
+import com.matheus.repository.CourseRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/courses")
+@AllArgsConstructor // lombok cria o construtor automaticamente
+public class CourseController {
+
+    private final CourseRepository courseRepository; // final diz que n podemos alterar o val dessa inst durante o life cycle
+
+    @GetMapping
+    public List<Course> list () {
+        return courseRepository.findAll();
+    }
 }
