433a0717c8718c5893f7d2d64c7c737d484aaac9
Matheus Ferreira <matheus.ferreira@bitka.com.br>
2023-03-30 17:58:42 -0300

    adicionando suporte a requisição POST no backend

--- crud_spring/src/main/java/com/matheus/controller/CourseController.java
+++ crud_spring/src/main/java/com/matheus/controller/CourseController.java
@@ -4,9 +4,9 @@ import com.matheus.model.Course;
 import com.matheus.repository.CourseRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,4 +21,12 @@ public class CourseController {
     public List<Course> list () {
         return courseRepository.findAll();
     }
+
+    @PostMapping // C(create)
+    public ResponseEntity<Course> create(@RequestBody Course course ){ //requestBody faz com que os atributos se encaixem
+        //System.out.println(hero.getName());
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(courseRepository.save(course )); //esse retorno mostra para o desenvolvedor o status da requisição
+    }
+
 }
