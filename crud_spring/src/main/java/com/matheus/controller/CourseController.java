7660ecb95c83251274e8815ff26277301016a98e
Matheus Ferreira <matheus.ferreira@bitka.com.br>
2023-04-04 10:34:15 -0300

    criando função http de update no spring

--- crud_spring/src/main/java/com/matheus/controller/CourseController.java
+++ crud_spring/src/main/java/com/matheus/controller/CourseController.java
@@ -22,7 +22,7 @@ public class CourseController {
         return courseRepository.findAll();
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("/{id}") // R(read by id)
     public ResponseEntity<Course> findById(@PathVariable Long id) { // @PathVariable indica que vem uma variavel no caminho da API
         return courseRepository.findById(id)
                 .map(record -> ResponseEntity.ok().body(record))
@@ -34,6 +34,18 @@ public class CourseController {
         //System.out.println(hero.getName());
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(courseRepository.save(course )); //esse retorno mostra para o desenvolvedor o status da requisição
+    }
+
+    @PutMapping("/{id}") // U(update)
+    public ResponseEntity<Course> update(@PathVariable Long id, @RequestBody Course course) {
+        return courseRepository.findById(id)
+                .map(recordFound -> {
+                    recordFound.setName(course.getName());
+                    recordFound.setCategory(course.getCategory());
+                    Course updated = courseRepository.save(recordFound);
+                    return ResponseEntity.ok().body(updated);
+                })
+                .orElse(ResponseEntity.notFound().build());
     }
 
 }
