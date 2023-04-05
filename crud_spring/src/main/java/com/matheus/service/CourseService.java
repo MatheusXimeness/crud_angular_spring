09717608e6ea5f845dca1f5d86e18d1fc5e47785
Matheus Ferreira <matheus.ferreira@bitka.com.br>
2023-04-05 18:34:22 -0300

    colocando execeções na camada de serviços

--- crud_spring/src/main/java/com/matheus/service/CourseService.java
+++ crud_spring/src/main/java/com/matheus/service/CourseService.java
@@ -1,5 +1,6 @@
 package com.matheus.service;
 
+import com.matheus.exception.RecordNotFoundException;
 import com.matheus.model.Course;
 import com.matheus.repository.CourseRepository;
 import org.springframework.http.HttpStatus;
@@ -29,29 +30,28 @@ public class CourseService {
         return courseRepository.findAll();
     }
 
-    public Optional<Course> findById(@PathVariable @NotNull @Positive Long id) {
-        return courseRepository.findById(id);
+    public Course findById(@PathVariable @NotNull @Positive Long id) {
+        return courseRepository.findById(id)
+                .orElseThrow(() -> new RecordNotFoundException(id));
     }
 
     public Course create(@Valid Course course ) {
         return courseRepository.save(course );
     }
 
-    public Optional<Course> update(@NotNull @Positive Long id, @Valid Course course) {
+    public Course update(@NotNull @Positive Long id, @Valid Course course) {
         return courseRepository.findById(id)
                 .map(recordFound -> {
                     recordFound.setName(course.getName());
                     recordFound.setCategory(course.getCategory());
                     return courseRepository.save(recordFound);
-                });
+                }).orElseThrow(() -> new RecordNotFoundException(id));
     }
 
-    public boolean delete(@PathVariable @NotNull @Positive Long id) {
-        return courseRepository.findById(id)
-                .map(recordFound -> {
-                    courseRepository.deleteById(id);
-                    return true;
-                })
-                .orElse(false);
+    public void delete(@PathVariable @NotNull @Positive Long id) {
+
+        courseRepository.delete(courseRepository.findById(id)
+                .orElseThrow(() -> new RecordNotFoundException(id))
+        );
     }
 }
