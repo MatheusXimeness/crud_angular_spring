c228e0596cd43f3e59974a49599f1df4eaecf9af
Matheus Ferreira <matheus.ferreira@bitka.com.br>
2023-04-05 16:24:11 -0300

    criando lógica de remoção lógica com Hibernate

--- crud_spring/src/main/java/com/matheus/model/Course.java
+++ crud_spring/src/main/java/com/matheus/model/Course.java
@@ -2,6 +2,8 @@ package com.matheus.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 import org.hibernate.validator.constraints.Length;
 
 import javax.persistence.*;
@@ -12,6 +14,8 @@ import javax.validation.constraints.Pattern;
 @Data
 @Entity // nesse caso utilizando entity é criada uma tabela com o nome dessa classe no BD
 //@Table(name = "cursos") - isso é p/ caso ja tenha um bd legado e vc deseja ref sua table
+@SQLDelete(sql = "UPDATE Course SET status = 'Inativo' WHERE id = ?") // toda vez que eu tentar remover um registro, oq vai acontecer na verdade é que ele será atualizado
+@Where(clause = "status = 'Ativo'") // toda vez que eu fizer um select na base de dados o Hibernate vai fazer esse append dentro do filtro
 public class Course {
 
     @Id
@@ -30,4 +34,10 @@ public class Course {
     @Pattern(regexp = "Back-end|Front-end")
     @Column(length = 10, nullable = false)
     private String category;
+
+    @NotNull
+    @Length( max = 10)
+    @Pattern(regexp = "Ativo|Inativo")
+    @Column(length = 10, nullable = false)
+    private String status = "Ativo";
 }
