c38d56ce80a73a5f3f44cbaec23b9cd8049cdbe7
Matheus Ferreira <matheus.ferreira@bitka.com.br>
2023-04-05 17:40:08 -0300

    migrando chamadas do courseRepository para a camada de serviços

--- crud_spring/src/main/java/com/matheus/controller/CourseController.java
+++ crud_spring/src/main/java/com/matheus/controller/CourseController.java
@@ -2,8 +2,7 @@ package com.matheus.controller;
 
 import com.matheus.model.Course;
 import com.matheus.repository.CourseRepository;
-import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.matheus.service.CourseService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -17,29 +16,33 @@ import java.util.List;
 @Validated
 @RestController
 @RequestMapping("/api/courses")
-@AllArgsConstructor // lombok cria o construtor automaticamente
+//@AllArgsConstructor // lombok cria o construtor automaticamente
 public class CourseController {
 
-    private final CourseRepository courseRepository; // final diz que n podemos alterar o val dessa inst durante o life cycle
+    private final CourseService courseService; // final diz que n podemos alterar o val dessa inst durante o life cycle
+
+    public CourseController(CourseService courseService) {
+        this.courseService = courseService;
+    }
 
     // -------- CREATE -------- //
     @PostMapping // C(create)
     public ResponseEntity<Course> create(@RequestBody @Valid Course course ){ //requestBody indica que estou trazendo parte de um curso
         //System.out.println(hero.getName());
         return ResponseEntity.status(HttpStatus.CREATED)
-                .body(courseRepository.save(course )); //esse retorno mostra para o desenvolvedor o status da requisição
+                .body(courseService.create(course )); //esse retorno mostra para o desenvolvedor o status da requisição
     }
     // -------- -------- //
 
     // -------- READ -------- //
     @GetMapping // R(read)
-    public List<Course> list () {
-        return courseRepository.findAll();
+    public @ResponseBody List<Course> list () {
+        return courseService.list();
     }
 
     @GetMapping("/{id}") // R(read by id)
-    public ResponseEntity<Course> findById(@PathVariable Long id) { // @PathVariable indica que vem uma variavel no caminho da API
-        return courseRepository.findById(id)
+    public ResponseEntity<Course> findById(@PathVariable @NotNull @Positive Long id) { // @PathVariable indica que vem uma variavel no caminho da API
+        return courseService.findById(id)
                 .map(record -> ResponseEntity.ok().body(record))
                 .orElse(ResponseEntity.notFound().build());
     }
@@ -49,13 +52,8 @@ public class CourseController {
     // -------- UPDATE -------- //
     @PutMapping("/{id}") // U(update)
     public ResponseEntity<Course> update(@PathVariable @NotNull @Positive Long id, @RequestBody @Valid Course course) {
-        return courseRepository.findById(id)
-                .map(recordFound -> {
-                    recordFound.setName(course.getName());
-                    recordFound.setCategory(course.getCategory());
-                    Course updated = courseRepository.save(recordFound);
-                    return ResponseEntity.ok().body(updated);
-                })
+        return courseService.update(id, course)
+                .map(recordFound -> ResponseEntity.ok().body(recordFound) )
                 .orElse(ResponseEntity.notFound().build());
     }
     // -------- -------- //
@@ -63,12 +61,10 @@ public class CourseController {
     // -------- DELETE -------- //
     @DeleteMapping("/{id}") // D(delete)
     public ResponseEntity<Void> delete(@PathVariable @NotNull @Positive Long id) {
-        return courseRepository.findById(id)
-                .map(recordFound -> {
-                    courseRepository.deleteById(id);
-                    return ResponseEntity.noContent().<Void>build();
-                })
-                .orElse(ResponseEntity.notFound().build());
+        if( courseService.delete(id) ) {
+            return ResponseEntity.noContent().<Void>build();
+        }
+        return ResponseEntity.notFound().build();
     }
     // -------- -------- //
 
