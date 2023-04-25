7d75f49345b7ce13beffa40199f35c446c62b1fd
Matheus Ferreira <matheus.ferreira@bitka.com.br>
2023-04-25 16:31:03 -0300

    criando funções que mapeiam de curso para DTO e de DTO para curso

--- crud_spring/src/main/java/com/matheus/dto/mapper/CourseMapper.java
+++ crud_spring/src/main/java/com/matheus/dto/mapper/CourseMapper.java
@@ -1,2 +1,32 @@
-package com.matheus.dto.mapper;public class CourseMapper {
+package com.matheus.dto.mapper;
+
+import com.matheus.dto.CourseDTO;
+import com.matheus.model.Course;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CourseMapper {
+
+    public CourseDTO toDTO( Course course ) {
+        if(course ==  null) {
+            return null;
+        }
+        return new CourseDTO(course.getId(), course.getName(), course.getCategory());
+    }
+
+    public Course toEntity( CourseDTO courseDTO ) {
+
+        if(courseDTO ==  null) {
+            return null;
+        }
+
+        Course course = new Course();
+        if (courseDTO.id() != null) {
+            course.setId(courseDTO.id());
+        }
+        course.setName(courseDTO.name());
+        course.setCategory(courseDTO.category());
+        return course;
+    }
+
 }
