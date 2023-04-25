99503290ea0f6a4595f99082965edd95d6f44959
Matheus Ferreira <matheus.ferreira@bitka.com.br>
2023-04-25 11:17:49 -0300

    Criando classe record para DTO e instanciando seu construtor

--- crud_spring/src/main/java/com/matheus/dto/CourseDTO.java
+++ crud_spring/src/main/java/com/matheus/dto/CourseDTO.java
@@ -1,2 +1,17 @@
-package com.matheus.dto;public record CourseDTO() {
+package com.matheus.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+// record é uma classe imutavel do java pois a partir do momento que vc instancia o construtor, esses serão os dados.
+public record CourseDTO(
+        @JsonProperty("_id") Long id,
+        @NotNull @NotBlank @Length(min = 5, max = 100) String nome,
+        @NotNull @Length( max = 10) @Pattern(regexp = "Back-end|Front-end") String category) {
+
+
 }
