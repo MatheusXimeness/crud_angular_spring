29d1af72d50b00e399f259d63f48e44761a3d45d
Matheus Ferreira <matheus.ferreira@bitka.com.br>
2023-04-25 16:32:04 -0300

    fazendo com que o serviço não lide diretamente com o modelo de entidade, mas sim chame o Mapper para fazer isso

--- crud_spring/src/main/java/com/matheus/service/CourseService.java
+++ crud_spring/src/main/java/com/matheus/service/CourseService.java
@@ -1,50 +1,53 @@
 package com.matheus.service;
 
+import com.matheus.dto.CourseDTO;
+import com.matheus.dto.mapper.CourseMapper;
 import com.matheus.exception.RecordNotFoundException;
-import com.matheus.model.Course;
 import com.matheus.repository.CourseRepository;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
 import java.util.List;
-import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Validated
 @Service
 public class CourseService {
 
     private final CourseRepository courseRepository;
+    private final CourseMapper courseMapper;
 
-    public CourseService(CourseRepository courseRepository) {
+    public CourseService(CourseRepository courseRepository, CourseMapper courseMapper) {
         this.courseRepository = courseRepository;
+        this.courseMapper = courseMapper;
     }
 
-    public List<Course> list () {
-        return courseRepository.findAll();
+    public List<CourseDTO> list () {
+        return courseRepository.findAll()
+                .stream()// captura os registros para que se possa fazer a programação reativa
+                .map(courseMapper::toDTO)// para cada curso do findAll é chamado a função toDTO
+                .collect(Collectors.toList()); // retorna uma list
     }
 
-    public Course findById(@PathVariable @NotNull @Positive Long id) {
-        return courseRepository.findById(id)
+    public CourseDTO findById(@PathVariable @NotNull @Positive Long id) {
+        return courseRepository.findById(id).map(courseMapper::toDTO)
                 .orElseThrow(() -> new RecordNotFoundException(id));
     }
 
-    public Course create(@Valid Course course ) {
-        return courseRepository.save(course );
+    public CourseDTO create(@Valid @NotNull CourseDTO course ) {
+        return courseMapper.toDTO(courseRepository.save(courseMapper.toEntity(course)));
     }
 
-    public Course update(@NotNull @Positive Long id, @Valid Course course) {
+    public CourseDTO update(@NotNull @Positive Long id, @Valid @NotNull CourseDTO course) {
         return courseRepository.findById(id)
-                .map(recordFound -> {
-                    recordFound.setName(course.getName());
-                    recordFound.setCategory(course.getCategory());
-                    return courseRepository.save(recordFound);
+                .map( recordFound -> {
+                    recordFound.setName(course.name());
+                    recordFound.setCategory(course.category());
+                    return courseMapper.toDTO(courseRepository.save(recordFound));
                 }).orElseThrow(() -> new RecordNotFoundException(id));
     }
 
