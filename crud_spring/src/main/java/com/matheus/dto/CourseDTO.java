35d48789a439492c560acb870f4769a5fe507647
Matheus Ferreira <matheus.ferreira@bitka.com.br>
2023-04-25 16:27:13 -0300

    corrigindo nome do atributo

--- crud_spring/src/main/java/com/matheus/dto/CourseDTO.java
+++ crud_spring/src/main/java/com/matheus/dto/CourseDTO.java
@@ -10,7 +10,7 @@ import javax.validation.constraints.Pattern;
 // record é uma classe imutavel do java pois a partir do momento que vc instancia o construtor, esses serão os dados.
 public record CourseDTO(
         @JsonProperty("_id") Long id,
-        @NotNull @NotBlank @Length(min = 5, max = 100) String nome,
+        @NotNull @NotBlank @Length(min = 5, max = 100) String name,
         @NotNull @Length( max = 10) @Pattern(regexp = "Back-end|Front-end") String category) {
 
 
