79ae986088e81be595fb791c50c7ac1c2fe31e2a
Matheus Ferreira <matheus.ferreira@bitka.com.br>
2023-05-16 15:30:31 -0300

    Inserindo lista de aulas no DTO de course

--- crud_spring/src/main/java/com/matheus/dto/CourseDTO.java
+++ crud_spring/src/main/java/com/matheus/dto/CourseDTO.java
@@ -1,17 +1,21 @@
 package com.matheus.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.matheus.model.Lesson;
 import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
+import java.util.List;
 
 // record é uma classe imutavel do java pois a partir do momento que vc instancia o construtor, esses serão os dados.
 public record CourseDTO(
         @JsonProperty("_id") Long id,
         @NotNull @NotBlank @Length(min = 5, max = 100) String name,
-        @NotNull @Length( max = 10) @Pattern(regexp = "Back-end|Front-end") String category) {
+        @NotNull @Length( max = 10) @Pattern(regexp = "Back-end|Front-end") String category,
+
+        List<Lesson> lessons) {
 
 
 }
