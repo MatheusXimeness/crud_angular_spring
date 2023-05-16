14a7b8537e9567f1b5318be36335e4430615d473
Matheus Ferreira <matheus.ferreira@bitka.com.br>
2023-05-16 15:08:56 -0300

    Criando novo model Lesson que tem seus atributos e além disse uma notação (manyToOne) que faz com que tenhamos uma relação biderecional com o Course

--- crud_spring/src/main/java/com/matheus/model/Lesson.java
+++ crud_spring/src/main/java/com/matheus/model/Lesson.java
@@ -1,2 +1,26 @@
-package com.matheus.model;public class Lesson {
+package com.matheus.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+public class Lesson {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(length = 100, nullable = false)
+    private String name;
+
+    @Column(length = 11, nullable = false)
+    private String youtubeUrl;
+
+    @ManyToOne(fetch = FetchType.LAZY, // somenente quando for acionado o getCourse é que esse mapeamento sera chamado
+               optional = false)
+    @JoinColumn(name = "course_id", nullable = false)
+    private Course course;
 }
